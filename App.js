587892477f06bc8169c74e08c5a0fb99c12a7926
philipp <philipp.reiter90@gmail.com>
2024-07-08 10:19:49 +0200

    init commit

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useVideoPlayer, VideoView } from 'expo-video';
+import { useRef, useState } from 'react';
+import {  StyleSheet, View, Button } from 'react-native';
+
+const videoSource =
+  'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_30MB.mp4';
 
 export default function App() {
+  const ref = useRef(null);
+  const player = useVideoPlayer(videoSource, (player) => {
+    player.loop = true;
+    player.play();
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.contentContainer}>
+      <VideoView
+      ref={ref}
+      style={styles.video}
+      player={player}
+      contentFit="fill"
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  contentContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 10,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 50,
+  },
+  video: {
+    width: 350,
+    height: 275,
+    backgroundColor: 'yellow',
+  },
+  controlsContainer: {
+    padding: 10,
   },
 });

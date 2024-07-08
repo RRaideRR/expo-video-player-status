33ed25bc651fd963a9047913a4ed16b91e70cebb
philipp <philipp.reiter90@gmail.com>
2024-07-08 18:10:51 +0200

    init commit

--- App.js
+++ App.js
@@ -3,38 +3,45 @@ import { useRef, useEffect } from 'react';
 import { StyleSheet, View, Button, useWindowDimensions } from 'react-native';
 
 const videoSource =
-  'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h265/1080/Big_Buck_Bunny_1080_10s_30MB.mp4';
+  'https://download.samplelib.com/mp4/sample-5s.mp4';
 
 const videoSource2 =
-  'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h265/1080/Big_Buck_Bunny_1080_10s_30MB.mp4';
+  'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4';
 
 export default function App() {
   const player1 = useVideoPlayer(videoSource, (player1) => {
+    player1.muted = true;
     player1.loop = true;
     player1.play();
   });
 
   const player2 = useVideoPlayer(videoSource2, (player2) => {
+    player2.muted = true;
     player2.loop = true;
     player2.play();
   });
 
+  const {width, height} = useWindowDimensions();
+
   return (
     <View style={{flex: 1}}>
-      <VideoView
-        player={player1}
-        contentFit="cover"
-        style={{width: '100%', height: '100%'}}
-        nativeControls={false}
-      />
+      <View style={{position: 'absolute',  height: height, width: width, zIndex: 5, elevation: 5}}>
+        <VideoView
+          player={player1}
+          contentFit="cover"
+          style={{width: '100%', height: '100%'}}
+          nativeControls={false}
+        />
+      </View>
 
-      <VideoView
-        player={player2}
-        contentFit="contain"
-        style={{width: 300, height: 200}}
-        nativeControls={false}
-      />
+      <View style={{position: 'absolute', backgroundColor: 'yellow'}}>
+        <VideoView
+          player={player2}
+          contentFit="contain"
+          style={{width: 300, height: 200}}
+          nativeControls={false}
+        />
+      </View>
     </View>
-
   );
 }

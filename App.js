b19cc3a77e39c45f55f4b1296977d1844dd1ed26
philipp <philipp.reiter90@gmail.com>
2024-07-08 14:50:39 +0200

    init commit

--- App.js
+++ App.js
@@ -1,43 +1,40 @@
 import { useVideoPlayer, VideoView } from 'expo-video';
-import { useRef, useState } from 'react';
-import {  StyleSheet, View, Button } from 'react-native';
+import { useRef, useEffect } from 'react';
+import { StyleSheet, View, Button, useWindowDimensions } from 'react-native';
 
 const videoSource =
-  'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_30MB.mp4';
+  'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h265/1080/Big_Buck_Bunny_1080_10s_30MB.mp4';
+
+const videoSource2 =
+  'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h265/1080/Big_Buck_Bunny_1080_10s_30MB.mp4';
 
 export default function App() {
-  const ref = useRef(null);
-  const player = useVideoPlayer(videoSource, (player) => {
-    player.loop = true;
-    player.play();
+  const player1 = useVideoPlayer(videoSource, (player1) => {
+    player1.loop = true;
+    player1.play();
+  });
+
+  const player2 = useVideoPlayer(videoSource2, (player2) => {
+    player2.loop = true;
+    player2.play();
   });
 
   return (
-    <View style={styles.contentContainer}>
+    <View style={{flex: 1}}>
       <VideoView
-      ref={ref}
-      style={styles.video}
-      player={player}
-      contentFit="fill"
+        player={player1}
+        contentFit="cover"
+        style={{width: '100%', height: '100%'}}
+        nativeControls={false}
+      />
+
+      <VideoView
+        player={player2}
+        contentFit="contain"
+        style={{width: 300, height: 200}}
+        nativeControls={false}
       />
     </View>
+
   );
 }
-
-const styles = StyleSheet.create({
-  contentContainer: {
-    flex: 1,
-    padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 50,
-  },
-  video: {
-    width: 350,
-    height: 275,
-    backgroundColor: 'yellow',
-  },
-  controlsContainer: {
-    padding: 10,
-  },
-});
